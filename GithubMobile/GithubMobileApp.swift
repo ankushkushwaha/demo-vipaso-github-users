615a69a6f56020bbab3da3a129d9a7904bfe5d45
ankush <ankush7377@gmail.com>
2024-06-15 15:28:19 +0530

    Initial Commit

---
+++ GithubMobile/GithubMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubMobileApp.swift
+//  GithubMobile
+//
+//  Created by Ankush Kushwaha on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
