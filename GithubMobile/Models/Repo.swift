0fe0056e72ac97e6a6a6c0724cd43939e26c7183
Ankush <ankush7377@gmail.com>
2024-10-02 16:48:54 +0530

    Rearrange and handle optioanl repo properties

--- GithubMobile/Models/Repo.swift
+++ GithubMobile/Models/Repo.swift
@@ -10,18 +10,18 @@ import Foundation
 struct Repo: Codable, Equatable {
     let id: Int
     let name: String
-    let language: String
-    let stargazersCount: String
-    let forkCount: String
-    let description: String
+    let language: String?
+    let description: String?
+    let stargazersCount: Int
+    let forkCount: Int
 
     enum CodingKeys: String, CodingKey {
         case id
         case name
         case language
+        case description
         case stargazersCount = "stargazers_count"
         case forkCount = "forks_count"
-        case description
     }
 }
 
