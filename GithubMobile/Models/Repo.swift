d169c5e081338b39901a2865b8e2d3ed9191d4e3
Ankush <ankush7377@gmail.com>
2024-10-02 16:32:55 +0530

    Add more properties in repo model

--- GithubMobile/Models/Repo.swift
+++ GithubMobile/Models/Repo.swift
@@ -10,10 +10,24 @@ import Foundation
 struct Repo: Codable, Equatable {
     let id: Int
     let name: String
-    
+    let language: String
+    let stargazersCount: String
+    let forkCount: String
+    let description: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case language
+        case stargazersCount = "stargazers_count"
+        case forkCount = "forks_count"
+        case description
+    }
+}
+
+extension Repo {
     static func == (lhs: Repo, rhs: Repo) -> Bool {
         return lhs.id == rhs.id &&
         lhs.name == rhs.name
     }
 }
-
