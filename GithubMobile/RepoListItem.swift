1c534740f94a13d6e176d5e76a223a4ab5248335
Ankush <ankush7377@gmail.com>
2024-10-02 17:49:27 +0530

    Refactor repo item view

---
+++ GithubMobile/RepoListItem.swift
@@ -0,0 +1,54 @@
+//
+//  RepoListItem.swift
+//  GithubMobile
+//
+//  Created by Ankush Kushwaha on 02/10/24.
+//
+
+import SwiftUI
+
+struct RepoListItem: View {
+    let repo: Repo
+    var body: some View {
+        VStack(spacing: 8) {
+            HStack(alignment: .top, spacing: 5) {
+                Text(repo.name)
+                    .font(.system(size: 15, weight: .medium))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+                Text("Language: \(repo.language ?? "-")" )
+                    .font(.system(size: 15, weight: .light))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+            }
+            
+            HStack(alignment: .top, spacing: 5) {
+                Text("Stars: \(repo.stargazersCount)")
+                    .font(.system(size: 15, weight: .light))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+                Text("Forks: \(repo.forkCount)")
+                    .font(.system(size: 15, weight: .light))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+            }
+            
+            if repo.isForked {
+                Text("Forked")
+            }
+        }
+        .frame(maxWidth: .infinity)  // Expands VStack to take full width
+    }
+}
+
+#Preview {
+    RepoListItem(
+        repo: Repo(
+            id: 1,
+            name: "Repo",
+            language: "swift",
+            description: "des",
+            stargazersCount: 1,
+            forkCount: 2,
+            isForked: false
+        )
+    )
+}
